ab2f5487cc0dfd9a1bc81a92ef71d82ee652861b
Amit Raj <amit@portfolio.dev>
2026-05-23 00:07:58 +0530

    Add GitHub Pages deployment workflow and configure Next.js for static export

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  basePath: '/amit-portfoliio',
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
